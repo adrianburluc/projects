7541e25c36fe91bbe6878725516ee8f86ed548fc
Adrian Burluc <76481170+adrianburluc@users.noreply.github.com>
2024-01-13 09:48:22 +0200

    Add files via upload

---
+++ hotel_management_system/project/ClassRepository/PatCamera.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRepository
+{
+    public class PatCamera
+    {
+        public int idCategorieCamera { get; set; }
+        public int idPat { get; set; }
+        public string denumirePat { get; set; }
+        public int numarPaturi { get; set; }
+
+        public PatCamera(int idCategorieCamera, int idPat, string denumirePat, int numarPaturi)
+        {
+            this.idCategorieCamera = idCategorieCamera;
+            this.idPat = idPat;
+            this.denumirePat = denumirePat;
+            this.numarPaturi = numarPaturi;
+        }
+    }
+}
